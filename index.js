afa39d4c09c4c9b491aa0054a79a28583f7227f1
AlbertoBallesterosRivas <ballesterosrivasa@gmail.com>
2022-07-26 12:11:38 +0200

    Part 3 exercise 3.5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 let persons = [
   {
     id: 1,
@@ -53,10 +53,29 @@ app.get("/api/persons/:id", (request, response) => {
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
+  persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end;
-})
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number || false,
+    id: Math.random() * (999999999999 - 0) + 0,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(request.body);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
