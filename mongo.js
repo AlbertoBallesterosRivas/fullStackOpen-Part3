379633d2303fdecedb0ba27aa9f1da5391e7d2c2
AlbertoBallesterosRivas <ballesterosrivasa@gmail.com>
2022-08-07 14:00:01 +0200

    Part 3 exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullStack:${password}@cluster0.qhbe2ex.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (3 in process.argv || 4 in process.argv) {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+      });
+
+      return person.save();
+    })
+    .then(() => {
+      console.log(
+        `added ${process.argv[3]} number ${process.argv[4]} to phonebook`
+      );
+      return mongoose.connection.close();
+    })
+    .catch((err) => console.log(err));
+} else {
+  mongoose
+    .connect(url)
+    .then(() => {
+      console.log("phonebook:");
+      Person.find({}).then((result) => {
+        result.forEach((person) => {
+          console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+      });
+    })
+    .catch((err) => console.log(err));
+}
