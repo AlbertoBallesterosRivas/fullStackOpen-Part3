b6ddeb5540822118769d84230de9969f196e3bd8
AlbertoBallesterosRivas <ballesterosrivasa@gmail.com>
2022-08-08 11:05:10 +0200

    Part 3 exercise 3.16

--- index.js
+++ index.js
@@ -34,15 +34,16 @@ app.get("/info", (request, response) => {
     `);
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -75,3 +76,23 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+// handler of requests with result to errors
+app.use(errorHandler)
