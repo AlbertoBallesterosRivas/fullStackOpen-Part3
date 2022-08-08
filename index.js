804483e81e59246c83a445582349da7760f0dd6d
AlbertoBallesterosRivas <ballesterosrivasa@gmail.com>
2022-08-08 11:30:14 +0200

    Part 3 exercise 3.18

--- index.js
+++ index.js
@@ -1,90 +1,93 @@
 const express = require("express");
 const app = express();
-app.use(express.static('build'));
+app.use(express.static("build"));
 app.use(express.json());
 
-require('dotenv').config()
-const Person = require('./models/person')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const Person = require("./models/person");
+const mongoose = require("mongoose");
 
 // DO NOT SAVE YOUR PASSWORD TO GITHUB!!
 const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log("connecting to", url);
 
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 app.get("/api/persons", (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
   let current = new Date();
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
+  Person.countDocuments({}).then((count) => {
+    response.send(`
+    <p>Phonebook has info for ${count} people</p>
     <p>${current.toUTCString()}</p>
     `);
+  });
 });
 
 app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
+    .then((result) => {
+      response.status(204).end();
     })
-    .catch(error => next(error))
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
   if (body.name === undefined) {
-    return response.status(400).json({ error: 'name missing' })
+    return response.status(400).json({ error: "name missing" });
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  })
+  });
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
-app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
 
   const person = {
     name: body.name,
     number: body.number,
-  }
+  };
 
   Person.findByIdAndUpdate(request.params.id, person, { number: body.number })
-    .then(updatedPerson => {
-      response.json(updatedPerson)
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
@@ -92,22 +95,22 @@ app.listen(PORT, () => {
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 // handler of requests with unknown endpoint
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
 
-  next(error)
-}
+  next(error);
+};
 
 // this has to be the last loaded middleware.
 // handler of requests with result to errors
-app.use(errorHandler)
+app.use(errorHandler);
